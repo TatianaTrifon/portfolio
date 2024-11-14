c4cc8f4aa19f421e591233c5ef4597b72d5f7ff3
TaneaT <tatiana.trifon03@gmail.com>
2024-11-14 11:47:33 +0200

    Added the image views and their functionality in sign up page

--- fit_plan/src/main/java/org/example/fit_plan/controllers/SignUpController.java
+++ fit_plan/src/main/java/org/example/fit_plan/controllers/SignUpController.java
@@ -4,18 +4,26 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 
 import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
 import org.example.fit_plan.dao.implimentations.UserDAOImpl;
 import org.example.fit_plan.model.User;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
 
-public class SignUpController {
+public class SignUpController implements Initializable {
+    @FXML
+    private ImageView backImage, exitImage, menuImage;
+
 
     @FXML
     private TextField emailField;
@@ -85,4 +93,22 @@ public class SignUpController {
         stage.show();
     }
 
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        Image back = new Image("file:/C:\\Users\\User\\IdeaProjects\\portfolio\\fit_plan\\src\\main\\java\\org\\example\\fit_plan\\images\\back.png");
+        backImage.setImage(back);
+
+        backImage.setOnMouseClicked(e -> {
+            try {
+                goToSignIn(new ActionEvent(backImage, backImage.getScene().getWindow()));
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        Image exit = new javafx.scene.image.Image("file:/C:\\Users\\User\\IdeaProjects\\portfolio\\fit_plan\\src\\main\\java\\org\\example\\fit_plan\\images\\exit.png");
+        exitImage.setImage(exit);
+        exitImage.setOnMouseClicked(event -> System.exit(0));
+
+    }
 }
